976d805476fbf771b3f085c98599a8e6ff250ba7
emrecanpolat <35192029+emrecanpolat@users.noreply.github.com>
2023-02-21 21:37:30 +0300

    Update deneme.php

--- deneme.php
+++ deneme.php
@@ -3,21 +3,6 @@
 error_reporting(E_ALL);
 
 
-/*if ($_POST['sayi'] %3 == 0){
-    echo "girdiğiniz sayi:".$_POST['sayi']." 3'e bölünebilir.";
-}elseif($_POST['sayi']==""){
-    echo "lütfen bir değer giriniz";
-}
-else{
-    echo "girdiğiniz sayi:".$_POST['sayi']." 3'e bölünemez."."<br>";
-    $deger = $_POST['sayi'];
-    while ($deger %3 != 0){
-        $deger+=1;
-
-    }
-    echo "3'e bölünecek en yakın sayı:" . $deger; 
-
-}*/
 if ($_POST['sayi']==""){
     echo "lütfen bir değer giriniz";
 }elseif($_POST['sayi'] %3 == 0){
